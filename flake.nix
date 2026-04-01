5335b997f9b0d89947000d23dd8fa9e90a0d1554
Ibrahim Johar <56348868+ibbejohar@users.noreply.github.com>
2026-04-01 21:42:33 +0200

    For building dwm on nixos

--- flake.nix
+++ flake.nix
@@ -1,59 +1,72 @@
 {
-  description = "Flake to build dwl from a custom GitHub repo";
+  description = "Custom dwm build";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # Your custom dwl repo as a flake input
-    dwl-custom.url = "github:ibbejohar/dwl";
+    # Your custom dwm repo
+    dwm-custom.url = "github:ibbejohar/dwm";
   };
 
-  outputs = { self, nixpkgs, dwl-custom, ... }:
+  outputs = { self, nixpkgs, dwm-custom, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      packages.${system}.dwl = pkgs.stdenv.mkDerivation {
-        pname = "dwl";
+      packages.${system}.dwm = pkgs.stdenv.mkDerivation rec {
+        pname = "dwm";
         version = "custom";
 
-        # Use the source from the flake input
-        src = dwl-custom;
+        src = dwm-custom;
 
         nativeBuildInputs = [
-          pkgs.pkgconfig
-          pkgs.meson
-          pkgs.ninja
-          pkgs.wayland
-          pkgs.wlroots
-          pkgs.glib
-          pkgs.fontconfig
           pkgs.pkg-config
-          pkgs.git
-          pkgs.cairo
-          pkgs.pango
-          pkgs.libxkbcommon
-          pkgs.pixman
-          pkgs.libdrm
-          pkgs.libinput
-          pkgs.udev
         ];
 
+        buildInputs = [
+          pkgs.xorg.libX11
+          pkgs.xorg.libXft
+          pkgs.xorg.libXinerama
+
+          # XCB support (for your modified dwm)
+          pkgs.xorg.libxcb
+          pkgs.xorg.xcbutil
+          pkgs.xorg.xcbutilwm
+          pkgs.xorg.xcbutilkeysyms
+          pkgs.xorg.xcbutilrenderutil
+
+          pkgs.freetype
+          pkgs.fontconfig
+        ];
+
+        # Fix hardcoded paths in config.mk if present
+        postPatch = ''
+          if [ -f config.mk ]; then
+            sed -i "s|/usr/X11R6|${pkgs.xorg.libX11.dev}|g" config.mk
+          fi
+        '';
+
         buildPhase = ''
-          meson setup build
-          meson compile -C build
+          runHook preBuild
+          make
+          runHook postBuild
         '';
 
         installPhase = ''
-          meson install -C build
+          runHook preInstall
+          mkdir -p $out/bin
+          cp dwm $out/bin/
+          runHook postInstall
         '';
 
         meta = with pkgs.lib; {
-          description = "Dynamic Wayland compositor based on dwm";
+          description = "Personal dwm build";
           license = licenses.mit;
-          maintainers = with maintainers; [ ];
           platforms = platforms.linux;
         };
       };
+
+      # Optional: make `nix build` default to this
+      defaultPackage.${system} = self.packages.${system}.dwm;
     };
 }
