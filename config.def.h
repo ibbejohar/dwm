d83eda1083a6baada7796f51bafd4af80f64e86a
Ibrahim Johar <ibbe.johar@gmail.com>
2026-09-13 11:43:36 +0200

    changing terminal and adding a missing field on rules

--- config.def.h
+++ config.def.h
@@ -29,11 +29,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title            tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",     NULL,       NULL,            0,         1,          0,           0,        -1 },
-	{ "Firefox",  NULL,       NULL,            1 << 8,    0,          0,          -1,        -1 },
-	{ "St",       NULL,       NULL,            0,         0,          1,           0,        -1 },
-	{ NULL,       NULL,       "Event Tester",  0,         0,          0,           1,        -1 }, /* xev */
+	/* class      instance    title            tags mask  isfloating  isfullscreen isterminal  noswallow  monitor */
+	{ "Gimp",     NULL,       NULL,            0,         1,          0,           0,          0,         -1 },
+	{ "Firefox",  NULL,       NULL,            1 << 8,    0,          0,           0,          0,         -1 },
+	{ "Alacritty", "Alacritty", NULL,          0,         0,          0,           1,          0,         -1 },
+	{ NULL,       NULL,       "Event Tester",  0,         0,          0,           1,          0,         -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
